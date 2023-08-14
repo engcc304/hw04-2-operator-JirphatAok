7fcdf0cc34cbeb664bbc41fe76ee09796a67bad4
Jirphat Saetae <jirphat.aok@gmail.com>
2023-08-14 21:40:18 +0700

    402

--- Homework/HW04/hw04-2-operator.cpp
+++ Homework/HW04/hw04-2-operator.cpp
@@ -1,7 +1,7 @@
 /*
     จงเติมโค้ดโปรแกรมตามที่แสดงในส่วนของ Placeholder เพื่อแสดงผลลัพธ์ของ Operator ด้านซ้ายมือ พร้อมทั้งแก้ไขโค้ดโปรแกรมให้ถูกต้อง
 */
-#include <stduio.h>
+#include <stdio.h>
 int main() {
     int a = 0, b = 0 ;
     printf( "Please enter value (a b) : " ) ;
@@ -11,17 +11,17 @@ int main() {
     printf( "a = %d , b = %d \n", a, b ) ;
     printf( "a + b = %d\n", a + b ) ;
     printf( "a - b = %d\n", a - b ) ;
-    printf( "a * b = %d\n", a * b ) 
+    printf( "a * b = %d\n", a * b ) ;
     printf( "a / b = %d (b must greater than 0)\n", b > 0 ? a : 0 ) ;
     printf( "a % b = %d\n", a % b ) ;
     printf( "++a = %d\n", ++a ) ;
     printf( "a-- = %d\n", a-- ) ;
     printf( "a += 1 %d\n", a += 1 ) ;
     printf( "a += b %d\n", a += b) ;
-    printf( "a -= 1 %d\n", a -= 1 ) :
+    printf( "a -= 1 %d\n", a -= 1 ) ;
     printf( "a -= b %d\n", a -= b ) ;
     printf( "a *= 1 %d\n", a *= 1 ) ;
-    print ( "a *= b %d\n", a *= b ) ;
+    printf ( "a *= b %d\n", a *= b ) ;
     printf( "a %= 1 %d\n", a %= 1 ) ;
     printf( "a %= b %d\n", a %= b ) ;
     printf( "a && b %d\n", a && b ) ;
